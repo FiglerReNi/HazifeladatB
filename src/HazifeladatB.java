b0a9d1a94e28c7c67da6d72bebf486b886290406
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2020-04-09 20:27:14 +0200

    Add files via upload
    forras

---
+++ src/HazifeladatB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FReni
+ */
+public class HazifeladatB {
+    public static void main(String[]args){
+        for(int i=1; i<=20; i++){
+        System.out.print(i*i+" ");
+    }
+    System.out.println();
+    System.out.println("Köszönjük");
+}
+}
